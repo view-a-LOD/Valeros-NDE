913a1d4565924a2bc7a6685b5a71bdf1c603c9e6
Simon Dirks <mail@simondirks.com>
2026-03-25 16:14:20 +0100

    Enable CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true,
+  });
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
 
