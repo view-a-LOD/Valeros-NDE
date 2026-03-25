f8d69fb165088a91de01b4dd68649df88683600d
Simon Dirks <mail@simondirks.com>
2026-03-25 14:32:20 +0100

    Add basic LDkit search function

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { createLens, Lens, SchemaInterface, type Options } from 'ldkit';
+import { rdfs } from 'ldkit/namespaces';
+
+const NodeSchema = {
+  label: rdfs.label,
+} as const;
+
+type NodeType = SchemaInterface<typeof NodeSchema>;
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  async getData(): Promise<NodeType[]> {
+    const searchHits: NodeType[] = await this.search(
+      'https://api.triplydb.com/datasets/academy/pokemon/sparql',
+      'Growl',
+    );
+    return searchHits;
+  }
+
+  async search(endpoint: string, searchTerm?: string): Promise<NodeType[]> {
+    const options: Options = {
+      sources: [endpoint],
+    };
+
+    const lens: Lens<NodeType> = createLens(NodeSchema, options);
+
+    if (searchTerm) {
+      const results: NodeType[] = await lens.find({
+        where: {
+          label: { $contains: searchTerm },
+        },
+      });
+
+      return results;
+    }
+
+    const results: NodeType[] = await lens.find({ take: 10 });
+    return results;
   }
 }
