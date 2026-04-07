0e7c2dddfc1fc6f1b033e0eee3b2e6a0c64dfd7e
Simon Dirks <mail@simondirks.com>
2026-04-07 16:13:51 +0200

    Retrieve data by ID for details page

--- apps/web/src/app/features/details/components/details-page/details.component.ts
+++ apps/web/src/app/features/details/components/details-page/details.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { SearchApiService } from '../../../search/services/search-api.service';
+import { SearchNode } from '@valeros-ldkit/shared-types';
 
 @Component({
   selector: 'app-details',
@@ -11,13 +13,49 @@ import { CommonModule } from '@angular/common';
 export class DetailsComponent implements OnInit {
   id: string | null = null;
   decodedId: string | null = null;
+  data = signal<SearchNode | null>(null);
+  loading = signal<boolean>(false);
+  error = signal<string | null>(null);
 
   private route = inject(ActivatedRoute);
+  private searchApiService = inject(SearchApiService);
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
     if (this.id) {
       this.decodedId = decodeURIComponent(this.id);
+      this.fetchData();
     }
+  }
+
+  private fetchData() {
+    if (!this.decodedId) return;
+
+    this.loading.set(true);
+    this.error.set(null);
+
+    const filters = JSON.stringify({
+      '@id': { $eq: this.decodedId },
+    });
+
+    this.searchApiService
+      .search({
+        query: '',
+        filters: filters,
+      })
+      .subscribe({
+        next: (response) => {
+          if (response.results.length > 0) {
+            this.data.set(response.results[0]);
+          } else {
+            this.error.set('No data found for this ID');
+          }
+          this.loading.set(false);
+        },
+        error: (err) => {
+          this.error.set('Failed to load data: ' + err.message);
+          this.loading.set(false);
+        },
+      });
   }
 }
