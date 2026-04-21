c200d3cb8f4e6456e2c11ba5a959ed51e7476f78
Simon Dirks <mail@simondirks.com>
2026-04-21 15:35:50 +0200

    Update is-part-of-object.ts

--- src/app/shared/types/node/is-part-of-object.ts
+++ src/app/shared/types/node/is-part-of-object.ts
@@ -1,14 +1,6 @@
 import { NodeModel } from './node.model';
 
 export type IsPartOfObject = NodeModel & {
-  publisher?: {
-    id: string;
-    type: string;
-    name: string;
-  };
-  license?: {
-    id: string;
-    type: string;
-    name: string;
-  };
+  publisher?: NodeModel;
+  license?: NodeModel;
 };
