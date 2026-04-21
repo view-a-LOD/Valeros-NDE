948e95fc50ce59cdca3f6ade19424ae3d52b737f
Simon Dirks <mail@simondirks.com>
2026-04-21 16:51:19 +0200

    Add fallback image + text when images fail to load

--- src/app/shared/components/widgets/image-gallery-widget/image-gallery-widget.component.ts
+++ src/app/shared/components/widgets/image-gallery-widget/image-gallery-widget.component.ts
@@ -70,9 +70,27 @@ export class ImageGalleryWidget extends BaseWidget implements OnDestroy {
       gallery: `#${this.galleryId}`,
       children: 'a',
       pswpModule: () => import('photoswipe'),
+      errorMsg: 'Er ging iets mis bij het laden van de afbeelding',
     });
+    // this.lightbox.addFilter(
+    //   'contentErrorElement',
+    //   (contentErrorElement, content) => {
+    //     const el = document.createElement('div');
+    //     el.className = 'pswp__error-msg';
+    //     el.innerHTML = `<img src="https://placehold.co/600x400.png" alt="Image unavailable" />`;
+    //     return el;
+    //   },
+    // );
+    // this.lightbox.addFilter('placeholderSrc', (placeholderSrc, content) => {
+    //   return 'https://placehold.co/600x400.png';
+    // });
 
     this.lightbox.init();
+  }
+
+  onThumbnailError(event: Event): void {
+    const img = event.target as HTMLImageElement;
+    img.src = 'https://placehold.co/600x400?text=X';
   }
 
   ngOnDestroy(): void {
