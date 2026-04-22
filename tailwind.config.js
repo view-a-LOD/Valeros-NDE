a0238cf2cd4fb1b25f1c7eb0a30795fd9b7308a4
Simon Dirks <mail@simondirks.com>
2026-04-22 14:22:14 +0200

    Tweak styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,22 @@ module.exports = {
       fontFamily: {
         sans: ['"Familjen Grotesk"', 'sans-serif'],
       },
+      colors: {
+        'app-bg': '#E1ECF2',
+        'app-bg-dark': '#D1DCE2',
+      },
     },
   },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ['light', 'dark'],
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/theming/themes')['light'],
+          primary: '#00839F',
+          'primary-content': '#ffffff',
+        },
+      },
+    ],
   },
 };
