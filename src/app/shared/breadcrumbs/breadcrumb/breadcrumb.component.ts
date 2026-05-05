d1ccb4572bbeec2d5581e88bcd4d7a0307688100
Simon Dirks <mail@simondirks.com>
2026-05-05 16:27:08 +0200

    Tweak mobile breadcrumbs styling

--- src/app/shared/breadcrumbs/breadcrumb/breadcrumb.component.ts
+++ src/app/shared/breadcrumbs/breadcrumb/breadcrumb.component.ts
@@ -1,10 +1,13 @@
 import { Component, inject, Signal } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { featherChevronRight } from '@ng-icons/feather-icons';
 import { BreadcrumbService, BreadcrumbItem } from '../breadcrumb.service';
 
 @Component({
   selector: 'app-breadcrumb',
-
+  imports: [NgIconComponent],
   templateUrl: './breadcrumb.component.html',
+  viewProviders: [provideIcons({ featherChevronRight })],
 })
 export class BreadcrumbComponent {
   private breadcrumbService = inject(BreadcrumbService);
